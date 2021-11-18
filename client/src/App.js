a8aad5bbcb695a671e365ceab9b35d2a3578da16
ImJiHwan1 <61612132+ImJiHwan1@users.noreply.github.com>
2021-11-18 16:10:03 +0900

    1118 수정

--- client/src/App.js
+++ client/src/App.js
@@ -14,36 +14,25 @@ const styles = theme => ({
         minWidth: 1080
     }
 })
-
-
-const customers = [
-    {
-        'id': 1,
-        'image': 'https://placeimg.com/64/64/1',
-        'name': '홍길동',
-        'birthday': '961222',
-        'gender': '남자',
-        'job': '대학생'
-    },
-    {
-        'id': 2,
-        'image': 'https://placeimg.com/64/64/2',
-        'name': '길동무',
-        'birthday': '961230',
-        'gender': '여자',
-        'job': '개발자'
-    },
-    {
-        'id': 3,
-        'image': 'https://placeimg.com/64/64/3',
-        'name': '길찾기',
-        'birthday': '960621',
-        'gender': '남자',
-        'job': '프로그래머'
-    },
-]
-
+    
 class App extends Component {
+
+    state = {
+        customers: ""
+    }
+
+    componentWillUnmount() {
+        this.callApi()
+            .then(res => this.setState({ customers: res }))
+            .catch(err => console.log(err))
+    }
+
+    callApi = async() => {
+        const response = await fetch('/api/customers')
+        const body = await response.json();
+        return body;
+    }
+
     render() {
         const { classes } = this.props;
 
@@ -61,7 +50,7 @@ class App extends Component {
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        { customers.map(c => {
+                        { this.state.customers ? this.state.customers.map(c => {
                             return (
                                 <Customer
                                     key={c.id}
@@ -74,6 +63,8 @@ class App extends Component {
                                 />
                             )
                         })
+                        :
+                            ""
                         }
                     </TableBody>
                 </Table>
--- server.js
+++ server.js
@@ -1,13 +1,37 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+var express = require('express');
 const app = express();
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-    res.send({ message: 'Hello Express!' });
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+            'id': 1,
+            'image': 'https://placeimg.com/64/64/1',
+            'name': '홍길동',
+            'birthday': '961222',
+            'gender': '남자',
+            'job': '대학생'
+        },
+        {
+            'id': 2,
+            'image': 'https://placeimg.com/64/64/2',
+            'name': '길동무',
+            'birthday': '961230',
+            'gender': '여자',
+            'job': '개발자'
+        },
+        {
+            'id': 3,
+            'image': 'https://placeimg.com/64/64/3',
+            'name': '길찾기',
+            'birthday': '960621',
+            'gender': '남자',
+            'job': '프로그래머'
+        },
+    ]);
 })
 
-app.listen(port, ()=> console.log(`Listenig on port ${port}`))
+app.listen(port, ()=> console.log(`Listening on port ${port}`))
